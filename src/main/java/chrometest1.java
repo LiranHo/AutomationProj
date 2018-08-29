21530a14df0a7f9eadbd4e92e5f161a380c06533
liran.hochman <liran345@gmail.com>
2018-08-29 17:19:01 +0300

    Initial commit 2

--- src/main/java/chrometest1.java
+++ src/main/java/chrometest1.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class chrometest1 extends Chrome {
 
@@ -6,5 +6,6 @@ public class chrometest1 extends Chrome {
     @Test
     public void newTest(){
         System.out.println("tset chrome 1");
+        client.sleep(1000);
     }
 }
